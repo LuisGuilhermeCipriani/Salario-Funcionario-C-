557d7637b386bb8487709166bd036458d1841885
Luis Cipriani <cipriani.luisguilherme@gmail.com>
2026-03-30 10:55:07 -0300

    Criação dos métodos da classe Worker

--- SalarioFuncionario/Entities/Worker.cs
+++ SalarioFuncionario/Entities/Worker.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using SalarioFuncionario.Entities.Enums;
 
 namespace SalarioFuncionario.Entities
 {
     internal class Worker
     {
         public string Name { get; set; }
-        public WorkerLevel level { get; set; }
+        public WorkerLevel Level { get; set; }
         public double BaseSalary { get; set; }
 
         public List<HourContract> Contracts { get; set; } = new List<HourContract>();
@@ -18,24 +19,34 @@ namespace SalarioFuncionario.Entities
         public Worker(string name, WorkerLevel level, double baseSalary, Department department)
         {
             Name = name;
-            WorkerLevel = workerLevel;
+            Level = level;
             BaseSalary = baseSalary;
             department = department;
         }
 
         public void AddContract(HourContract contract)
         {
-
+            Contracts.Add(contract);
         }
 
         public void Remove(HourContract contract)
         {
-
+            Contracts.Remove(contract);
         }
 
         public double Income(int year, int month)
         {
+            double sum = BaseSalary;
 
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+
+            return sum;
         }
 
     }
